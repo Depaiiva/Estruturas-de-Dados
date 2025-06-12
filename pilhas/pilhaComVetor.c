613dd1332328de69768c7140b34a44af78b5a561
Carlos Henrique <carlos.rique23@gmail.com>
2025-06-12 14:48:36 -0300

    Implementation of a stack on vector

--- pilhas/pilhaComVetor.c
+++ pilhas/pilhaComVetor.c
@@ -12,30 +12,40 @@ pilha *criarPilha(int N){
     p->dados = malloc(N * sizeof(int));
     p->N = N;
     p->topo = 0;
+    printf("Pilha criada");
     return p;
 }
 
 int empilhar(pilha *p, int x){
     if(p->N == p->topo){
         p->dados = realloc(p->dados, 2 * p->N * sizeof(int));
-        if(p->dados == NULL) return 1;
+        if(p->dados == NULL) {
+            printf("Erro...");
+            return 1;
+        }
         p->N = 2*p->N;
     }
     p->dados[p->topo] = x;
     p->topo++;
+    printf("Valor adicionado com sucesso!");
     return 0;
 }
 
 int desempilhar(pilha *p, int *y){
-    if(p->topo == 0) return 1;
+    if(p->topo == 0) {
+        printf("Erro...");
+        return 1;
+    }
     p->topo--;
     *y = p->dados[p->topo];
+    printf("Valor removido com sucesso!");
     return 0;
 }
 
 void detruir(pilha *p){
     free(p->dados);
     free(p);
+    printf("pilha destruida com sucesso!");
 }
 
 
