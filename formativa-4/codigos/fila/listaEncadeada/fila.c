f84eda7e400457478e1e654ae88a083339a2f447
Carlos Henrique <carlos.rique23@gmail.com>
2025-07-11 17:05:08 -0300

    implementação de filas atualizada

--- formativa-4/codigos/fila/listaEncadeada/fila.c
+++ formativa-4/codigos/fila/listaEncadeada/fila.c
@@ -1,3 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct celula{
+    int dado;
+    struct celula *prox;
+}fila;
+
+fila *criaFila(){
+    fila *cabeca = malloc(sizeof(fila));
+    if(cabeca == NULL) return NULL;
+    cabeca->prox = cabeca;
+    return cabeca;
+}
+
+fila *enfileira(fila *cabeca, int x){
+    fila *novaCabeca = malloc(sizeof(fila));
+    if(novaCabeca == NULL) return NULL;
+    novaCabeca->prox = cabeca->prox;
+    cabeca->prox = novaCabeca;
+    cabeca->dado = x;
+    return novaCabeca;
+}
